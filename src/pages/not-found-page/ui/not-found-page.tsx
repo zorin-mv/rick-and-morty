4ff8e396d586f938f916ee63319378f7b2ce7641
Maksym Zorin <zorin.maksym@gmail.com>
2023-04-03 16:37:41 +0400

    feature/not-found-page

--- src/pages/not-found-page/ui/not-found-page.tsx
+++ src/pages/not-found-page/ui/not-found-page.tsx
@@ -1,11 +1,11 @@
-import { classNames } from 'shared/lib/class-names';
-
-import classes from './not-found-page.module.scss';
+import { Banner } from 'widgets/banner';
 
 interface INotFoundPageProps {
   className?: string;
 }
 
 export const NotFoundPage = ({ className }: INotFoundPageProps) => (
-  <div className={classNames(classes.notFoundPage, [className])}>{'Page Not Found'}</div>
+  <div className={className}>
+    <Banner title="Page Not Found 404" />
+  </div>
 );
