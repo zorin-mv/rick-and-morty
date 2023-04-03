7d07465cc5a45123b3e36b34be123eb196e82a71
Maksym Zorin <zorin.maksym@gmail.com>
2023-04-03 12:31:57 +0400

    feature/page-header

---
+++ src/widgets/page-header/ui/page-header.tsx
@@ -0,0 +1,21 @@
+import { memo } from 'react';
+import { Link } from 'react-router-dom';
+import { RoutePath } from 'shared/constant/app-routes';
+import { classNames } from 'shared/lib/class-names';
+import { Icon } from 'shared/ui/icon';
+
+import classes from './page-header.module.scss';
+
+interface IPageHeaderProps {
+  className?: string;
+}
+
+export const PageHeader = memo(({ className }: IPageHeaderProps) => {
+  return (
+    <div data-testid="page-header" className={classNames(classes.pageHeader, [className])}>
+      <Link data-testid="link" to={RoutePath.main}>
+        <Icon data-testid="logo" type="logo" className={classes.logo} />
+      </Link>
+    </div>
+  );
+});
